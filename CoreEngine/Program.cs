8c5cd769ca49abb765d962fa871c068da5c5ed7d
Demi Oyeyemi <o.oyeyemi@lancaster.ac.uk>
2025-05-28 14:15:09 +0100

    Added functionality to produce random orders

--- CoreEngine/Program.cs
+++ CoreEngine/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Concurrent;
+using OrderMatching;
 
-Console.WriteLine("Hello, World!");
+namespace CoreEngine;
+
+class Program
+{
+    static void ProduceRandomOrders(BlockingCollection<Order> queue, CancellationToken token)
+    {
+        var rnd = new Random();
+
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var side = rnd.Next(2) == 0 ? Side.Buy : Side.Sell;
+                var price = Math.Round((decimal)(rnd.NextDouble() * 10 + 100), 2);
+                var qty = rnd.Next(1, 21);
+                var order = new Order(side, price, qty);
+                
+                queue.Add(order, token);
+                Console.WriteLine($"Enqueued {order}");
+                
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
