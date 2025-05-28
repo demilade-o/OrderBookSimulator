61e94f6217596f398bc8927f95e1ec8c5daa2953
Demi Oyeyemi <o.oyeyemi@lancaster.ac.uk>
2025-05-28 14:41:37 +0100

    Changed string output of Order

--- OrderMatching/Order.cs
+++ OrderMatching/Order.cs
@@ -22,5 +22,5 @@ namespace OrderMatching;
             Quantity = qty;
         }
 
-        public override string ToString() => $"{Side} {Price}@{Quantity} ({Id.ToString()[..8]})";
+        public override string ToString() => $"{Side} {Price} @ {Quantity} ({Id.ToString()[..8]})";
     }
